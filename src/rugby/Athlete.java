bc0bbb4960afe47f81d06fc9c477dfca1fa5ff99
liamceelent <liamceelent@gmail.com>
2023-05-04 21:19:09 +1200

    added team and athlete classes

---
+++ src/rugby/Athlete.java
@@ -0,0 +1,20 @@
+package rugby;
+
+public class Athlete {
+	
+	public String name;
+	public String nickName;
+	public int wage;
+	public int sellPrice;
+	public int buyPrice;
+	public int stamina;
+	public boolean injured;
+	public String position;
+	public int rarity;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
