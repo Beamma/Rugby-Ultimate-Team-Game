df3a1deb76692b4a06821402b8419829385f17b2
Beamma <jbremma@gmail.com>
2023-05-09 14:51:00 +1200

    Created updateMarket method in MarketPlace

--- src/rugby/MarketPlace.java
+++ src/rugby/MarketPlace.java
@@ -6,10 +6,27 @@ public class MarketPlace {
 	private ArrayList<Athlete> playersForSale = new ArrayList<Athlete>();
 	private ArrayList<Item> itemsForSale = new ArrayList<Item>();
 	
+	public void updateMarket() {
+		playersForSale.clear();
+		for (int i = 0; i < 5; i++) {
+			playersForSale.add(GameEnviroment.generatePlayer());
+		}
+	}
+	
+	public void buyObject(Object thing) {
+		if (this.moneyTransfer());
+	}
+	
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MarketPlace market = new MarketPlace();
+		market.updateMarket();
+		Athlete ed = GameEnviroment.generatePlayer();
+		market.buyObject(ed);
+		
+		
 	}
 
 }
