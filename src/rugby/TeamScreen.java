dd4135b778fefd3bff5e35608285e486678b4442
liamceelent <liamceelent@gmail.com>
2023-05-20 00:06:04 +1200

    athlete screen chnages

--- src/rugby/TeamScreen.java
+++ src/rugby/TeamScreen.java
@@ -12,6 +12,7 @@ import rugby.GameEnviroment;
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.TitledBorder;
 
@@ -60,7 +61,7 @@ public class TeamScreen {
 		
 		
 		JButton btnNewButton = new JButton("Home");
-		btnNewButton.setBounds(242, 413, 83, 23);
+		btnNewButton.setBounds(252, 410, 89, 23);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				goHome();
@@ -69,8 +70,8 @@ public class TeamScreen {
 		frame.getContentPane().setLayout(null);
 		frame.getContentPane().add(btnNewButton);
 
-		lblNewLabel = new JLabel("active team");
-		lblNewLabel.setBounds(93, 11, 77, 46);
+		lblNewLabel = new JLabel("Active team", SwingConstants.CENTER);
+		lblNewLabel.setBounds(46, 19, 172, 30);
 		frame.getContentPane().add(lblNewLabel);
 		
 		DefaultListModel<Athlete> athleteListModel = new DefaultListModel<Athlete>();
@@ -100,12 +101,12 @@ public class TeamScreen {
 				}
 			}
 		});
-		btnNewButton_1.setBounds(46, 372, 120, 23);
+		btnNewButton_1.setBounds(46, 356, 172, 23);
 		frame.getContentPane().add(btnNewButton_1);
 		
 		
-		JLabel lblNewLabel_1 = new JLabel("Reserve team");
-		lblNewLabel_1.setBounds(421, 27, 89, 14);
+		JLabel lblNewLabel_1 = new JLabel("Reserve team", SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(362, 27, 172, 14);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		DefaultListModel<Athlete> reserveModelList = new DefaultListModel<Athlete>();
@@ -113,7 +114,7 @@ public class TeamScreen {
 		
 		
 		JList<Athlete> reserveList = new JList<Athlete>();
-		reserveList.setBounds(386, 49, 148, 285);
+		reserveList.setBounds(372, 49, 172, 285);
 		frame.getContentPane().add(reserveList);
 		reserveList.setModel(reserveModelList);
 		
@@ -128,7 +129,7 @@ public class TeamScreen {
 				}
 			}
 		});
-		btnNewButton_2.setBounds(386, 372, 148, 23);
+		btnNewButton_2.setBounds(372, 356, 172, 23);
 		frame.getContentPane().add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("swap");
@@ -146,7 +147,7 @@ public class TeamScreen {
 				
 			}
 		});
-		btnNewButton_3.setBounds(242, 198, 89, 23);
+		btnNewButton_3.setBounds(252, 168, 89, 23);
 		frame.getContentPane().add(btnNewButton_3);
 
 	}
